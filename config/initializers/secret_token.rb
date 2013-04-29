158a3276f47c38710c11de61ae50eac6fb7e46ed
pc <pc@pc-desktop.(none)>
2013-04-29 10:38:18 +0800

    upload blank proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testfbapp::Application.config.secret_token = '9701e0a4bd9ba125f5aa6da8b432e79a692b2a9a912701ee3db9dd471bb6e5c83cf80473ecfab475d8f29cb186ce306a305091cecf3c307e6847fd1a3e7ce826'
